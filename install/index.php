1b8f957e5761f882a1597a2d48ad204e8b505fc5
Victor <victor@ebavs.net>
2013-04-10 22:15:14 +0200

    Installer

---
+++ install/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Refreak</title>
+<style>
+div, p, li, td {
+    font-family: Verdana, Arial, Helvetica, sans-serif;
+    font-size: small;
+}
+th {
+    font-family: Verdana, Arial, Helvetica, sans-serif;
+    font-size: small;
+    text-align: center;
+}
+big {
+    font-weight: bold;
+    font-size: x-large;
+}
+td input {
+    width: 120px;
+}
+#preview {
+    position: relative;
+    left: 50%;
+    width: 700px;
+    margin-left: -350px;
+    border: 2px solid #ccc;
+    padding: 5px;
+    background-color: #f3f3f3;
+}
+pre {
+    font-family: monospace;
+    font-size: normal;
+}
+.ok {
+    font-weight: bold;
+    color: #393;
+}
+.warning {
+	color: #f90;
+}
+.error {
+    font-weight: bold;
+    color: #c00;
+}
+</style>
+</head>
+
+
+<?php
+
+
+?>
