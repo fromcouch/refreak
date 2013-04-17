643d9797d83997cda80d2e3432156f3e657e579f
Victor <victor@ebavs.net>
2013-04-17 23:42:19 +0200

    Installer

--- install/index.php
+++ install/index.php
@@ -1,4 +1,5 @@
 <?php
+use \Michelf\Markdown;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 /**
@@ -10,9 +11,12 @@ ini_set('display_errors', 1);
  */
 include 'Install.php';
 include 'InstallDecorator.php';
+include 'Markdown.php';
+
 $inst = new Install();
 
-$install = true;
+$act = 0;
+if (isset($_GET['act'])) $act = $_GET['act'];
 ?>
 <!doctype html>
 <html lang="en">
@@ -65,46 +69,73 @@ pre {
     <p align="center"><img src="../skins/redfreak/images/freak-logo.png" border="0" alt="Refreak" /></p>
     <p align="center">
         <a href="#">Check install</a> |
-        <a href="?action=2">README!</a> |
+        <a href="?act=2">README!</a> |
         <a href="https://github.com/fromcouch/refreak/issues/" target="_blank">Support</a>
     </p>
     <div class="preview">
         
-        <p>Check configuration files</p>
-        <ul>
-            <?php
-                echo InstallDecorator::show_li_element('Check Refreak Config File', $inst->check_refreak_file());
-                echo InstallDecorator::show_li_element('Check Layout Config File', $inst->check_layout_file());
-                echo InstallDecorator::show_li_element('Check Application Config File', $inst->check_config_file());
-                echo InstallDecorator::show_li_element('Check Database Config File', $inst->check_database_file());
-            ?>
-        </ul>   
-        
-        <?php if ($inst->can_be_installed) : ?>
-            <p>Check database parameters</p>
-            <ul>
-                <?php
-                    echo InstallDecorator::show_li_element('Check Config Parameters', $inst->check_config_parameters());
-                    echo InstallDecorator::show_li_element('Check Database Parameters', $inst->check_database_parameters());
-                ?>
-            </ul>
-            <?php if ($inst->can_be_installed) : ?>
-                <p>Check database connection</p>
+        <?php 
+                
+        switch ($act) {
+            case 1:
+                // install tables here
+            
+            case 2: 
+                $text = file_get_contents('../README.md');
+                $html = Markdown::defaultTransform($text);
+                echo $html;
+                break;
+            
+            case 0:
+            default: ?>
+                <p>Check configuration files</p>
                 <ul>
                     <?php
-                        echo InstallDecorator::show_connection_state('Check Connection', $inst->check_connection(), $inst->connection_error);
+                        echo InstallDecorator::show_li_element('Check Refreak Config File', $inst->check_refreak_file());
+                        echo InstallDecorator::show_li_element('Check Layout Config File', $inst->check_layout_file());
+                        echo InstallDecorator::show_li_element('Check Application Config File', $inst->check_config_file());
+                        echo InstallDecorator::show_li_element('Check Database Config File', $inst->check_database_file());
                     ?>
-                </ul>
-                <?php if ($inst->can_be_installed) : ?>                    
-                    <input type="button" value="Install" name="install_button" class="install_button" />
+                </ul>   
+
+                <?php if ($inst->can_be_installed) : ?>
+                    <p>Check database parameters</p>
+                    <ul>
+                        <?php
+                            echo InstallDecorator::show_li_element('Check Config Parameters', $inst->check_config_parameters());
+                            echo InstallDecorator::show_li_element('Check Database Parameters', $inst->check_database_parameters());
+                        ?>
+                    </ul>
+                    <?php if ($inst->can_be_installed) : ?>
+                        <p>Check database connection</p>
+                        <ul>
+                            <?php
+                                echo InstallDecorator::show_connection_state('Check Connection', $inst->check_connection(), $inst->connection_error);
+                            ?>
+                        </ul>
+                        <?php if ($inst->can_be_installed) : ?>                    
+                            <input type="button" value="Install" name="install_button" class="install_button" />
+                        <?php else : ?>
+                            <p class="error">Can not continue without connecting to database</p>
+                        <?php endif; ?>
+                    <?php else : ?>
+                        <p class="error">Can not continue tests without access to config or database parameters</p>
+                    <?php endif; ?>
                 <?php else : ?>
-                    <p class="error">Can not continue without connecting to database</p>
-                <?php endif; ?>
-            <?php else : ?>
-                <p class="error">Can not continue tests without access to config or database parameters</p>
-            <?php endif; ?>
-        <?php else : ?>
-            <p class="error">Can not continue tests without access to config or database file</p>
-        <?php endif; ?>
+                    <p class="error">Can not continue tests without access to config or database file</p>
+                <?php endif; ?>        
+            <?php   
+                break;
+        } ?>     
+        
     </div>
+<script type="text/javascript">
+    (function () {
+        var ib = document.querySelector(".install_button");
+        ib.addEventListener("click", function() {
+            window.location.href="?act=1";
+        }, false);
+    })();
+</script>    
 </body>
+</html>
