d5344348f2c7b141c877d775c45de78722c267fa
Victor <victor@ebavs.net>
2013-04-22 18:11:49 +0200

    login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,47 @@
-<h1>Login</h1>
-<p>Please login with your email/username and password below.</p>
-	
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open("auth/login");?>
-  	
-  <p>
-    <label for="identity">Email/Username:</label>
-    <?php echo form_input($identity);?>
-  </p>
-
-  <p>
-    <label for="password">Password:</label>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <label for="remember">Remember Me:</label>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Refreak</title>
+        <meta http-equiv="content-type" content="text/html; charset=<?php echo config_item('charset');?>" />
+        <link rel="stylesheet" type="text/css" href="../theme/default/css/login.css" />
+</head>
+<body>
     
-    
-  <p><?php echo form_submit('submit', 'Login');?></p>
-    
-<?php echo form_close();?>
+    <div class="container">
+        <div class="horiz">
+            <h1>Login</h1>
+            <p>Please login with your email/username <br/>and password below.</p>
 
-<p><a href="forgot_password">Forgot your password?</a></p>
+            <div id="infoMessage"><?php echo $message;?></div>
+
+            <?php echo form_open("auth/login");?>
+
+              <p>
+                <label for="identity">Email/Username:</label>
+                <?php echo form_input($identity);?>
+              </p>
+
+              <p>
+                <label for="password">Password:</label>
+                <?php echo form_input($password);?>
+              </p>
+
+              <p>
+                <label for="remember">Remember Me:</label>
+                <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+              </p>
+
+
+              <p class="button"><?php echo form_submit('submit', 'Login');?></p>
+
+            <?php echo form_close();?>
+
+            <p><a href="forgot_password">Forgot your password?</a></p>
+
+        </div>
+    </div>
+    <footer class="footer">
+            Refreak! v0.1 Beta - 2013-04-18 - <a href="https://github.com/fromcouch/refreak/" target="_blank">Visit on Github</a>
+    </footer>
+</body>
+</html>
