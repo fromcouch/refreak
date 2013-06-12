6ed47a2f874173a4c34a7707dc7aebbf38331b4e
Victor <victor@ebavs.net>
2013-06-12 23:21:29 +0200

    plugin config controller

---
+++ application/controllers/plugin.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Plugin Controller
+ *
+ * @package	Refreak
+ * @subpackage	plugin
+ * @category	controller
+ * @author	Víctor <victor@ebavs.net> fromcouch
+ * @link	https://github.com/fromcouch/refreak
+ */
+class Plugin extends RF_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        
+    }
+    
+}
+
+?>
