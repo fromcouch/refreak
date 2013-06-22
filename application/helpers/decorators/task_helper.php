cb83dd1ac129655f13626f3b66e0d837ca491326
Victor <victor@ebavs.net>
2013-06-22 16:04:25 +0200

    task decorators

--- application/helpers/decorators/task_helper.php
+++ application/helpers/decorators/task_helper.php
@@ -76,11 +76,142 @@ class task_helper {
         
     }
     
-    public static function table_task_body_fields() {
+    /**
+     * Create table task content
+     * 
+     * @param array $context Context arrays
+     * @param array $tasks Task arrays
+     * @param string $theme_url Theme url
+     * @param bool $access permisions
+     * @return string task rows
+     * @access public
+     * @static
+     */
+    public static function table_task_body_fields($context, $tasks, $theme_url, $access, $actual_user_id) {
         
+        foreach ($tasks as $tf) {
+                
+                $tcol = array();
+                
+                //preparing some data
+                $context_letter     = substr($context[$tf->context], 0, 1);
+        
+                //priority 
+                $tcol []= '
+                    <td class="task_prio">
+                            <span class="task_pr' . $tf->priority . '">' . $tf->priority . '</span>
+                    </td>
+                ';
+                
+                //context
+                $tcol []= '
+                    <td class="task_ctsh">
+                            <span class="task_ctx' . $context_letter . '">' . $context_letter . '</span>
+                    </td>
+                ';
+                
+                //project name
+                $tcol []= '
+                    <td>' . $tf->project_name . '</td>
+                ';
+                
+                //title
+                $title = $tf->title;
+                
+                if (!empty($tf->description)) { //icon description
+                        $title .= '
+                            <img src="' . $theme_url . '/images/desc.png" width="16" height="16" 
+                                align="absmiddle" border="0" alt="" />
+                        ';
+                }
+                
+                if ($tf->private > 0) { // private icon
+                        $title .= '
+                            <img src="' . $theme_url . '/images/priv' . $tf->private . '.png" 
+                                width="12" height="16" align="absmiddle" border="0" alt="" />
+                        ';
+                }
+
+                $tcol []= '<td>' . $title . '</td>';
+
+                // first name
+                $tcol []= '<td>' . $tf->first_name . '</td>';
+                
+                //deadline
+                $tcol []= '<td>' . RFK_Task_Helper::calculate_deadline($tf->deadline_date, $tf->status_key) . '</td>';
+                
+                //comments
+                $tcol []= '
+                    <td>
+                        <div class="comment_count">
+                            ' . $tf->comment_count . '
+                        </div>
+                        <a href="#" class="comment_link">
+                            <img src="' . $theme_url . '/images/b_disc.png" width="14" height="16" alt="" border="0" />
+                        </a>
+                    </td>
+                ';
+                
+                //status
+                $stats = '';
+                for ($cont = 0; $cont < 5; $cont++) { 
+                    
+                    $sts = ($cont < $tf->status_key) ? (5 - $cont) : 0; 
+                    $status_class = 'sts'.$sts;
+                    if (RFK_Task_Helper::can_do($tf->task_id, $actual_user_id, $tf->position, $tf->author_id, 3)) {
+                        $status_class .= ' status'.$cont;
+                    }
+                    
+                    $stats .= '<td class="' . $status_class .'">&nbsp;</td>';
+                    
+                }
+                $tcol []= $stats;
+                
+                //buttons
+                if ($access || $tf->position > 3) {
+                        $buttons = '<a href="#" class="btn_task_edit">
+                                            <img src="' . $theme_url . '/images/b_edit.png" width="20" height="16" alt="edit" border="0" />
+                                   </a>
+                                   <a href="#" class="btn_task_delete">
+                                            <img src="' . $theme_url . '/images/b_dele.png" width="20" height="16" alt="del" border="0" />
+                                   </a>';
+                } else {
+                        $buttons = '<img src="' . $theme_url . '/images/b_edin.png" width="20" height="16" alt="del" border="0" />
+                                    <img src="' . $theme_url . '/images/b_deln.png" width="20" height="16" alt="del" border="0" />';
+                }
+                
+                $tcol []= '
+                    <td class="act"> 
+                        ' . $buttons . '
+                    </td>
+                ';
+                
+                $trow [] = '
+                    <tr data-id="' . $tf->task_id . '">
+                        ' . implode('', $tcol) . '
+                    </tr>
+                ';
+                
+                unset($tcol);
+
+        }
+        
+        return implode('', $trow);
+                            
     }
     
-    public static function table_no_task($no_task, $url, $new) {
+    /**
+     * No task table content
+     * 
+     * @param string $no_task No task message
+     * @param string $url url for images
+     * @param bool $access access to new task button
+     * @param string $new alternate for new task button image
+     * @return string No task table content
+     * @access public
+     * @static
+     */
+    public static function table_no_task($no_task, $url, $access, $new) {
      
         $tnotask = '<tr class="nothanks">
                         <td colspan="14">
--- application/views/tasks/tasks.php
+++ application/views/tasks/tasks.php
@@ -20,75 +20,22 @@
             <tbody class="taskSheetData">
                 <?php 
                     if (count($tasks)>0) :
-                        $context    = $this->lang->line('task_context');
-                        foreach ($tasks as $tf) : 
-                        //preparing some data
-                            $context_letter     = substr($context[$tf->context], 0, 1);
-                ?>
-                            <tr data-id="<?php echo $tf->task_id; ?>">
-                                <td class="task_prio">
-                                    <span class="task_pr<?php echo $tf->priority; ?>"><?php echo $tf->priority; ?></span>
-                                </td>
-                                <td class="task_ctsh">
-                                    <span class="task_ctx<?php echo $context_letter; ?>"><?php echo $context_letter; ?></span>
-                                </td>
-                                <td><?php echo $tf->project_name; ?></td>
-                                <td>
-                                    <?php 
-                                            echo $tf->title; 
-                                            if (!empty($tf->description)) : ?>
-                                                <img src="<?php echo base_url() . $theme;?>/images/desc.png" width="16" height="16" align="absmiddle" border="0" alt="" />
-                                            <?php endif;
-                                            if ($tf->private > 0) : ?>
-                                                <img src="<?php echo base_url() . $theme;?>/images/priv<?php echo $tf->private; ?>.png" width="12" height="16" align="absmiddle" border="0" alt="" />
-                                            <?php endif; ?>
-                                </td>
-                                <td><?php echo $tf->first_name; ?></td>
-                                <td><?php echo RFK_Task_Helper::calculate_deadline($tf->deadline_date, $tf->status_key); ?></td>
-                                <td>
-                                    <div class="comment_count">
-                                        <?php echo $tf->comment_count; ?>
-                                    </div>
-                                    <a href="#" class="comment_link">
-                                        <img src="<?php echo base_url() . $theme;?>/images/b_disc.png" width="14" height="16" alt="" border="0" />
-                                    </a>
-                                </td>
-                                <?php for ($cont = 0; $cont < 5; $cont++) : 
-                                            $sts = ($cont < $tf->status_key) ? (5 - $cont) : 0; 
-                                            $status_class = 'sts'.$sts;
-                                            if (RFK_Task_Helper::can_do($tf->task_id, $actual_user->id, $tf->position, $tf->author_id, 3)) :
-                                                $status_class .= ' status'.$cont;
-                                            endif;
-                                ?>
-                                <td class="<?php echo $status_class; ?>">&nbsp;</td>
-                                <?php endfor; ?>
-                                <td class="act">
-                                    <?php if ($this->ion_auth->in_group(array(1,2)) || $tf->position > 3) : ?>
-                                        <a href="#" class="btn_task_edit">
-                                            <img src="<?php echo base_url() . $theme;?>/images/b_edit.png" width="20" height="16" alt="edit" border="0" />
-                                        </a>
-                                    <?php else : ?>
-                                        <img src="<?php echo base_url() . $theme;?>/images/b_edin.png" width="20" height="16" alt="del" border="0" />
-                                    <?php endif;
-                                    // DELETE
-                                    if ($this->ion_auth->in_group(array(1,2))  || $tf->position > 3) : ?>
-                                        <a href="#" class="btn_task_delete">
-                                            <img src="<?php echo base_url() . $theme;?>/images/b_dele.png" width="20" height="16" alt="del" border="0" />
-                                        </a>
-                                    <?php else : ?>
-                                        <img src="<?php echo base_url() . $theme;?>/images/b_deln.png" width="20" height="16" alt="del" border="0" />
-                                    <?php endif; ?>
-                                </td>
-                            </tr>
-                <?php endforeach; 
-                else : 
+                        
+                        echo task_helper::table_task_body_fields($this->lang->line('task_context'), 
+                                                                 $tasks, 
+                                                                 base_url() . $theme, 
+                                                                 $this->ion_auth->in_group(array(1,2)),
+                                                                 $actual_user->id);
+                        
+                        
+                    else : 
                     
                         echo task_helper::table_no_task($this->lang->line('task_list_no_task'), 
                                                         base_url() . $theme, 
                                                         $this->ion_auth->in_group(array(1,2)), 
                                                         $this->lang->line('task_list_new'));
                     
-                endif; ?>
+                    endif; ?>
             </tbody>
     </table>
 <script type="text/javascript">
