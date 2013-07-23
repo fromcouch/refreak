055252e9206bf26acaded4a2f1a0467032d27fb6
Victor <Victor@victor-linka>
2013-07-23 08:30:36 +0200

    Revert "Issue #11 Solved"
    This reverts commit 6e9c6eaebd96250222d4e1597afba2655d341926.

--- application/models/project_model.php
+++ application/models/project_model.php
@@ -186,7 +186,7 @@ class Project_model extends CI_Model
     public function delete_task_status_by_project($project_id) {
 
         
-        $sql = 'delete rfk_task_status.* from `rfk_task_status`
+        $sql = 'delete from `rfk_task_status`
                 inner join  `rfk_tasks` on `rfk_tasks`.`task_id` = `rfk_task_status`.`task_id`
                 where `rfk_tasks`.`project_id` = ?';
 
