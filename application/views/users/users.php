377027996c26036b17e28322fa99ecd213830911
Victor <victor@ebavs.net>
2013-11-21 22:50:54 +0100

    url bug with php 5.4

--- application/views/users/users.php
+++ application/views/users/users.php
@@ -21,7 +21,7 @@
                 echo user_helper::table_user_body_fields($users, 
                                                          $this->ion_auth->is_admin(), 
                                                          base_url() . $theme, 
-                                                         site_url() . '/users/details/', 
+                                                         site_url() . 'users/details/', 
                                                          site_url() . 'users/delete_user/', 
                                                          site_url() . 'users/edit_user/', 
                                                          $actual_user->id, 
